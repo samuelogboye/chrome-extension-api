528a129e4437429dfa159f0fb188a3f044558b90
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-29 17:34:55 +0100

    run time updated

--- Extension/__init__.py
+++ Extension/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import os
+from flask_migrate import Migrate
 
 load_dotenv()
 
@@ -8,9 +10,13 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/videos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/videos.db'
 
 db = SQLAlchemy()
+
+migrate = Migrate(app, db)
+
 db.init_app(app)
 
 from Extension import routes
